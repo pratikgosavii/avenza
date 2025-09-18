5e62a4d738ce94caa9aa4c87be7132c0e8d3ecc8
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-09-19 01:14:09 +0530

    ddfdf

--- AVENZA/settings.py
+++ AVENZA/settings.py
@@ -260,4 +260,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
